0eee02cb9dfc26927b4572239372c552aa16a802
Matt Luna <mat.c.luna@gmail.com>
2021-12-04 21:33:37 -0600

    Login Form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,23 @@
 import gql from 'graphql-tag';
 
-export const Mutation_AddUser = gql`
-  mutation AddUser($username: String!, $email: String!, $password: String!) {
+export const ADD_USER = gql`
+  mutation adduser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
+        _id
         username
-        email
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        username
       }
     }
   }
